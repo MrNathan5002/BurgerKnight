9a5ee683871d4a9ef1598a612b4c453d63ebe66b
MrNathan5002 <nabuis4830@gmail.com>
2025-05-26 00:00:04 -0500

    Movement Update
    Adds movement and basic platforms to test

---
+++ Burger Knight/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 10f;
+    public float acceleration = 15f;
+    public float deceleration = 20f;
+    public float velocityPower = 0.9f;
+
+    [Header("Jump Settings")]
+    public float jumpForce = 15f;
+    public float jumpCutMultiplier = 0.5f;
+    public float coyoteTime = 0.15f;
+    public float jumpBufferTime = 0.1f;
+
+    [Header("Ground Detection")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+    private bool isJumpPressed;
+    private bool isJumpHeld;
+
+    private bool isGrounded;
+    private float lastTimeGrounded;
+    private float lastTimeJumpPressed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // --- Input ---
+        moveInput = Input.GetAxisRaw("Horizontal");
+        isJumpPressed = Input.GetButtonDown("Jump");
+        isJumpHeld = Input.GetButton("Jump");
+
+        // --- Timers ---
+        if (isJumpPressed) lastTimeJumpPressed = Time.time;
+        if (CheckGrounded()) lastTimeGrounded = Time.time;
+
+        // --- Jump ---
+        if (CanJump())
+        {
+            Jump();
+        }
+
+        // --- Variable Jump Height ---
+        if (!isJumpHeld && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpCutMultiplier);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // --- Horizontal Movement ---
+        float targetSpeed = moveInput * moveSpeed;
+        float speedDiff = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velocityPower) * Mathf.Sign(speedDiff);
+        rb.AddForce(Vector2.right * movement);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        lastTimeJumpPressed = 0;
+        lastTimeGrounded = 0;
+    }
+
+    bool CanJump()
+    {
+        return Time.time - lastTimeGrounded <= coyoteTime &&
+               Time.time - lastTimeJumpPressed <= jumpBufferTime;
+    }
+
+    bool CheckGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
