d0b4db9da9e94c8e61a2795fb49446bbde28d24a
MrNathan5002 <nabuis4830@gmail.com>
2025-06-04 20:46:05 -0500

    Fixed planting logic
    Can only plant while on the ground now

--- Burger Knight/Assets/Scripts/SeedPlanter.cs
+++ Burger Knight/Assets/Scripts/SeedPlanter.cs
@@ -7,9 +7,16 @@ public class SeedPlanter : MonoBehaviour
     public GameObject sesamePlantPrefab;
     public Transform plantPoint;
 
+    private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        playerMovement = GetComponentInParent<PlayerMovement>();
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && playerMovement.IsGrounded())
         {
             Instantiate(sesamePlantPrefab, plantPoint.position, Quaternion.identity);
         }
